dc7464d60c59ea1f52cfe4b3c7751d3117d87c7b
Alexandru Tache <hadesgames@gmail.com>
2013-07-10 11:36:15 +0100

    Added permissions to bin

--- src/main/java/uk/ac/cam/sup/models/Bin.java
+++ src/main/java/uk/ac/cam/sup/models/Bin.java
@@ -1,8 +1,13 @@
 package uk.ac.cam.sup.models;
 
+import org.hibernate.Session;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import uk.ac.cam.sup.HibernateUtil;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -12,7 +17,9 @@ public class Bin {
     private long id;
     private Set<BinPermission> permissions;
     private String question;
+    private String owner;
 
+    public Bin() {}
 
     //Getters
     @Id
@@ -24,6 +31,7 @@ public class Bin {
     public Set<BinPermission> getPermissions(){ return permissions; }
 
     public String getQuestion() { return question; }
+    public String getOwner() { return owner; }
 
 
     //Setters
@@ -32,7 +40,73 @@ public class Bin {
         this.permissions = permissions;
     }
     public void setQuestion(String question) { this.question = question; }
+    public void setOwner(String user) { this.question = question; }
 
+
+    // Actual useful functions
+    public boolean isDos(String user) {
+        return false;
+    }
+
+    public boolean isAdmin(String user) {
+        return false;
+    }
+
+    public boolean isOwner(String user) {
+        return user == owner;
+    }
+    /*
+    A dos can see anything
+    An admin can see anything
+    The supervisor can see anything
+    */
+
+    public boolean canSeeAll(String user) {
+        return isDos(user) || isAdmin(user) || isOwner(user);
+    }
+
+    /*
+    Only users with permissions can upload files.
+    TODO  Should the owner be able to upload ?!. (No atm)
+    */
+
+    public boolean canAddSubmission(String user) {
+        Session session = HibernateUtil.getSF().getCurrentSession();
+        Integer permission = (Integer) session.createCriteria(BinPermission.class)
+               .add(Restrictions.eq("user", user))
+               .setProjection(Projections.rowCount())
+               .list().get(0);
+
+        return (permission != 0);
+    }
+
+    /*
+    The Owner should be able to see any submissions
+    The Dos should be able to see any submissions
+    The Admin should be able to see any submissions
+    The User who uploaded the submission should be able to see it
+     */
+
+    public boolean canSeeSubmission(String user, Submission submission) {
+        if (isOwner(user) || isAdmin(user) || isDos(user)) {
+            return true;
+        }
+        return submission.getUser() == user;
+    }
+
+    /*
+    The Owner SHOULDN'T be able to delete the submission
+    The Dos SHOULDN'T be able to see delete the submission
+    The Admin should be able to delete the submission
+    The User who uploaded the submission should be able to delete it
+     */
+    public boolean canDeleteSubmission(String user, Submission submission) {
+        if (isAdmin(user)) {
+            return true;
+        }
+
+        return submission.getUser() == user;
+    }
 
 
 }
