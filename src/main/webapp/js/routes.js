b825477c3284756ad7ef5598c6ac9cc8102a9731
Alexandru Tache <hadesgames@gmail.com>
2013-07-16 13:18:29 +0100

    Added template for a bin

---
+++ src/main/webapp/js/routes.js
@@ -0,0 +1,17 @@
+/* Demo Routes:
+
+The first term must be the route from where the template will get the data.
+The second term must be either a string representing the template name or
+a function that returns the template name. The function will receive the json returned
+by the request as the first parameter.
+*/
+$(document).ready(function() {
+    router = Router({
+        //"tester": function(json) { return json['isSupervisor'] ? "a" : "b";}
+        // Use the last line to redirect unmatched routes to an error page
+        "submission/:id": "handins.submission.index"
+        //"*undefined": "main.test"
+    })
+})
+
+
