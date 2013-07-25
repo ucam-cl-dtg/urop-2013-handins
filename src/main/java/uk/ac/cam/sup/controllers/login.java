d2b07c01606128e46bcb94a212b5f672ef256d59
Andi Purice <andipurice@gmail.com>
2013-07-25 11:01:37 +0100

    Added login controller

--- src/main/java/uk/ac/cam/sup/controllers/login.java
+++ src/main/java/uk/ac/cam/sup/controllers/login.java
@@ -2,15 +2,19 @@ package uk.ac.cam.sup.controllers;
 
 import com.google.common.collect.ImmutableMap;
 import com.googlecode.htmleasy.ViewWith;
+import uk.ac.cam.sup.helpers.UserHelper;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
-@Path("/test")
+@Path("/")
 public class login {
     @GET
-    @ViewWith("/soy/main.index")
-    public Object bla(){
-        return ImmutableMap.of();
+    public Object bla(@FormParam("crsId") String crsId) {
+        UserHelper.currentUser = crsId;
+
+        return Response.ok().build();
     }
 }
