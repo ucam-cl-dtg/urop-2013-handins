b81b19860b67d7971c549310b2e6feaaccfacdf0
Alexandru Tache <hadesgames@gmail.com>
2013-08-02 10:41:52 +0100

    Added the old selector while I am working on the new one

--- src/main/webapp/js/handins/pdf-selector.js
+++ src/main/webapp/js/handins/pdf-selector.js
@@ -8,6 +8,15 @@
 */
 
 function addSelectingModal(bin, submission) {
+    var html = shared.handins.selectingModal2({bin: bin, submission: submission}),
+        elem = $(html);
+
+    elem.prependTo($('body')).foundation().foundation("reveal","open");
+    asyncLoad(elem.find(".async-loader"));
+    return elem;
+}
+
+function addSelectingModal2(bin, submission) {
     var html = shared.handins.selectingModal({bin: bin, submission: submission}),
         elem = $(html);
 
@@ -16,10 +25,32 @@ function addSelectingModal(bin, submission) {
     return elem;
 }
 
-
 function showSelectingModal(bin, submission) {
     $('#selectingModal').remove();
     addSelectingModal(bin, submission);
+    showPdf2(submission);
+    $('#selectingModal form').ajaxForm(function(){
+        $('#selectingModal').foundation("reveal", "close");
+    });
+
+    $('#selectingModal .more-inputs').click(function() {
+        var id = $('#selectingModal select').val();
+        var name = $('#selectingModal select option[value="' + id + '"]').text();
+        var start = $('#selectingModal input[name="start"]').val();
+        var end = $('#selectingModal input[name="end"]').val();
+
+        var elem = $(shared.handins.inputLine({
+            "question": { id: id, name: name},
+            "start": start,
+            "end": end
+        }))
+        elem.appendTo($('.input-line-container'))
+    });
+
+}
+function showSelectingModal2(bin, submission) {
+    $('#selectingModal').remove();
+    addSelectingModal2(bin, submission);
     showPdf(submission);
     $('#selectingModal form').ajaxForm(function(){
         $('#selectingModal').foundation("reveal", "close");
@@ -45,6 +76,34 @@ $(document).on("click", ".input-line a", function() {
     $(this).closest('.input-line').remove();
 })
 
+function showPdf2(submission) {
+    PDFJS.disableWorker = true;
+    PDFJS.getDocument("/submissions/" + submission + "/download").then(function (pdf){
+        var numPages = pdf.numPages;
+
+        for (var i=0; i < numPages; i++) {
+            $('<div class="pdf-page page-' + i + '"><canvas></canvas></div>' ).appendTo($('.pdf-container'));
+
+            pdf.getPage(i).then(function(i){ return function(page) {
+                var scale = 1;
+                var viewport = page.getViewport(scale);
+
+                var canvas = $(".page-" + i +" canvas")[0];
+                var context = canvas.getContext('2d');
+
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                page.render({canvasContext: context, viewport: viewport});
+
+            }}(i))
+
+        }
+
+    })
+}
+
+
 function showPdf(submission) {
     // Function located in viewer.js. That file contains magic, pure voodoo magic.
     loadPdf("/submissions/" + submission + "/download");
@@ -63,6 +122,7 @@ var Marker = (function MarkerClosure(){
 
         // Bind all events handlers to the marker
         this.startSelecting = bindThis(this.startSelecting, this);
+        this.stopSelecting = bindThis(this.stopSelecting, this);
         this.handleMouseMove = bindThis(this.handleMouseMove, this);
         this.handleResizableResize = bindThis(this.handleResizableResize, this);
         this.handleRescale = bindThis(this.handleRescale, this);
@@ -121,6 +181,9 @@ var Marker = (function MarkerClosure(){
     }
 
     Marker.prototype.startSelecting =  function(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
         $('.pdf-viewer .page').unbind("click", this.startSelecting);
 
         var elem = $(evt.currentTarget);
@@ -156,6 +219,13 @@ var Marker = (function MarkerClosure(){
 
         });
         $(window).on("scalechange", this.handleRescale);
+
+        this.pdfContainer.click(this.stopSelecting);
+    }
+
+    Marker.prototype.stopSelecting = function(evt) {
+        this.pdfContainer.unbind('click', this.stopSelecting);
+        this.pdfContainer.unbind('mousemove', this.handleMouseMove);
     }
 
     Marker.prototype.handleResizableResize =  function (evt, ui) {
