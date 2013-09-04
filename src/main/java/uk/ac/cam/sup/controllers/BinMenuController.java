d31090b067e38a151fbcc04d19a8b6d0bcd474cf
Andi Purice <andipurice@gmail.com>
2013-09-04 14:58:39 +0100

    Fixes #34.

--- src/main/java/uk/ac/cam/sup/controllers/BinMenuController.java
+++ src/main/java/uk/ac/cam/sup/controllers/BinMenuController.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.sup.controllers;
 
 import com.google.common.collect.ImmutableMap;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import java.util.LinkedList;
 import java.util.List;
@@ -28,30 +30,7 @@ public class BinMenuController {
     @GET
     @Path("/marking")
     public Object showBinsToMark() {
-
-        // Set Hibernate and get user
-        Session session = HibernateUtil.getSession();
-
-        String user = UserHelper.getCurrentUser(request);
-
-        // Get list of bins
-        @SuppressWarnings("unchecked")
-        List<Bin> binList = session.createCriteria(Bin.class)
-                                   .createAlias("accessPermissions", "perm")
-                                   .add(Restrictions.eq("perm.userCrsId", user))
-                                   .addOrder(Order.desc("id"))
-                                   .list();
-
-        // Filter all visible bins and return them
-        List<Map<String, ?>> finalBinList = new LinkedList<>();
-        for (Bin bin : binList)
-            if (bin.canAddMarkedSubmission(user))
-                finalBinList.add(ImmutableMap.of("id", bin.getId(),
-                                                 "name", bin.getName(),
-                                                 "isArchived", bin.isArchived(),
-                                                 "questions", bin.getQuestionCount()));
-
-        return ImmutableMap.of("bins", finalBinList);
+        return searchBins(null, null, false, null, null, true);
     }
 
     /*
@@ -60,29 +39,7 @@ public class BinMenuController {
     @GET
     @Path("/upload")
     public Object viewBinsToUpload() {
-
-        // Set Hibernate and get user
-        Session session = HibernateUtil.getSession();
-
-        String user = UserHelper.getCurrentUser(request);
-
-        // Get list of bins
-        @SuppressWarnings("unchecked")
-        List<Bin> binList = session.createCriteria(Bin.class)
-                                   .createAlias("accessPermissions", "perm")
-                                   .add(Restrictions.eq("perm.userCrsId", user))
-                                   .addOrder(Order.desc("id"))
-                                   .list();
-
-        // Filter all visible bins and return them
-        List<Map<String, ?>> finalBinList = new LinkedList<>();
-        for (Bin bin : binList)
-            finalBinList.add(ImmutableMap.of("id", bin.getId(),
-                                             "name", bin.getName(),
-                                             "isArchived", bin.isArchived(),
-                                             "questions", bin.getQuestionCount()));
-
-        return ImmutableMap.of("bins", finalBinList);
+        return searchBins(null, null, false, null, null, null);
     }
 
 
@@ -92,6 +49,20 @@ public class BinMenuController {
     @GET
     @Path("/manage")
     public Object viewBinsOwned() {
+        return searchBins(null, UserHelper.getCurrentUser(request), false, null, null, null);
+    }
+
+    /*
+    Done
+     */
+    @GET
+    @Path("/search")
+    public Object searchBins(@QueryParam("name") String queryName,
+                             @QueryParam("owner") String queryOwner,
+                             @QueryParam("archived") Boolean queryArchived,
+                             @QueryParam("offset") Integer queryOffset,
+                             @QueryParam("limit") Integer queryLimit,
+                             @QueryParam("markable") Boolean queryMarkable) {
 
         // Set Hibernate and get user
         Session session = HibernateUtil.getSession();
@@ -100,18 +71,36 @@ public class BinMenuController {
 
         // Get list of bins
         @SuppressWarnings("unchecked")
-        List<Bin> binList = session.createCriteria(Bin.class)
-                                   .add(Restrictions.eq("owner", user))
-                                   .addOrder(Order.desc("id"))
-                                   .list();
+        Criteria binCriteria = session.createCriteria(Bin.class)
+                                      .createAlias("accessPermissions", "perm")
+                                      .add(Restrictions.eq("perm.userCrsId", user))
+                                      .addOrder(Order.desc("id"));
+
+        // ToDo: fuzzySearch maybe?
+        if (queryName != null)
+            binCriteria.add(Restrictions.eq("name", queryName));
+
+        if (queryArchived != null)
+            binCriteria.add(Restrictions.eq("isArchived", queryArchived));
+        if (queryOwner != null)
+            binCriteria.add(Restrictions.eq("owner", queryOwner));
+        if (queryOffset != null)
+            binCriteria.setFirstResult(queryOffset);
+        if (queryLimit != null)
+            binCriteria.setMaxResults(queryLimit);
+
+        // Get list of bins
+        @SuppressWarnings("unchecked")
+        List<Bin> binList = binCriteria.list();
 
         // Filter all visible bins and return them
         List<Map<String, ?>> finalBinList = new LinkedList<>();
         for (Bin bin : binList)
-            finalBinList.add(ImmutableMap.of("id", bin.getId(),
-                                             "name", bin.getName(),
-                                             "isArchived", bin.isArchived(),
-                                             "questions", bin.getQuestionCount()));
+            if (queryMarkable == null || (queryMarkable && bin.canAddMarkedSubmission(user)))
+                finalBinList.add(ImmutableMap.of("id", bin.getId(),
+                                                 "name", bin.getName(),
+                                                 "isArchived", bin.isArchived(),
+                                                 "questions", bin.getQuestionCount()));
 
         return ImmutableMap.of("bins", finalBinList);
     }
